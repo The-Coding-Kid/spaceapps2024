84130ea382f96027aa7308e2829ac5bf683e57cd
ProximaProgrammer <64300638+ProximaProgrammer@users.noreply.github.com>
2024-10-23 04:14:18 -0700

    more useful navbar

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -13,26 +13,13 @@ const Navbar = ({
         className="hover:text-white duration-300 transition-all" >
         Lock on planet
       </button>
-      <Link className="hover:text-white duration-300 transition-all" href="/">
-        Interact
-      </Link>
       <Link
         className="hover:text-white duration-300 transition-all"
-        href="/featured"
-      >
-        Featured
+        href="/featured">
+        Surface View
       </Link>
-      <Link
-        className="hover:text-white duration-300 transition-all"
-        href="/about"
-      >
-        About
-      </Link>
-      <Link
-        className="hover:text-white duration-300 transition-all"
-        href="/calculation"
-      >
-        Calculation
+      <Link className="hover:text-white duration-300 transition-all" href="https://www.figma.com/proto/IM8vlxQbN1oC2q29MvDUJi/Nasa-Space-Apps-2024?node-id=1-2&node-type=canvas&m=dev&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2">
+        Website
       </Link>
     </div>
   );
